ca34fb1189b304442b5bd55b446da2db8df36643
Kavya Kavuri <kk1069@scarletmail.rutgers.edu>
2022-01-08 01:53:29 -0500

    build files

---
+++ precache-manifest.c99cb1937e59f970e9476585d61cb0ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "954153438d2d0f04b049",
+    "url": "/static/css/main.0d65f952.chunk.css"
+  },
+  {
+    "revision": "954153438d2d0f04b049",
+    "url": "/static/js/main.4d259d3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "69721db7013dcad70e67",
+    "url": "/static/js/2.df392bae.chunk.js"
+  },
+  {
+    "revision": "4eef928551d75e38ee5e81d833b168c9",
+    "url": "/index.html"
+  }
+];
